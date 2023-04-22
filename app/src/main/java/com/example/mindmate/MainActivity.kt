b28d3c244052c5994d36c2e15334fa19e67171a8
Henrique de Oliveira Ramos <henriqueramos.qs@gmail.com>
2023-04-21 21:41:11 -0300

    feat: Login activity and intent

--- app/src/main/java/com/example/mindmate/MainActivity.kt
+++ app/src/main/java/com/example/mindmate/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.mindmate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.mindmate.databinding.ActivityMainBinding
+import com.example.mindmate.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val loginBtt: Button = binding.loginBtt
+        loginBtt.setOnClickListener {
+            Intent(this,LoginActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
